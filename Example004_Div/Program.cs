d8db738c5270b70776eb1488def30adc2e42ada2
Indira Ivleva <indira.ivleva@gmail.com>
2022-08-08 16:37:51 +0300

    add Example 004 Div

---
+++ Example004_Div/Program.cs
@@ -0,0 +1,8 @@
+﻿using static System.Console;
+WriteLine("Введите число А ");
+double numberA = Convert.ToDouble(ReadLine());
+WriteLine("Введите число B ");
+double numberB = Convert.ToDouble(ReadLine());
+double result = numberA / numberB;
+Write("Частное равно ");
+Write(result);
