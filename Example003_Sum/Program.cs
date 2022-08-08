f75de90f23ce3282de5ad3d155fba1d47aad9e6b
Indira Ivleva <indira.ivleva@gmail.com>
2022-08-08 16:12:31 +0300

    add Example 003 Sum

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,8 @@
+﻿using static System.Console;
+WriteLine("Введите число А "); 
+int numberA = Convert.ToInt32(ReadLine());
+WriteLine("Введите число B ");
+int numberB = Convert.ToInt32(ReadLine());
+int result = numberA + numberB;
+Write("Сумма равна ");
+Write(result);
